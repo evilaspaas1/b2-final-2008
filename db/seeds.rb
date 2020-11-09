ec39aefb1f8aea3ecedd550f3b506e14b2424dce
Austin Aspaas <araspaas@live.com>
2020-11-09 09:43:49 -0700

    Add migrations, models, and model tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+hospital1 = Hospital.create(name: "Sweedish American")
+hospital2 = Hospital.create(name: "St. Anthony's")
+
+# Doctors:
+
+jim = hospital1.doctors.create(name: "Jim", speciality: "General Surgery", school: "Harvard University")
+brian = hospital1.doctors.create(name: "Brian", speciality: "Pediatric Surgery", school: "Johns Hopkins University")
+alex = hospital2.doctors.create(name: "Alex", speciality: "General Surgery", school: "Stanford University")
+tim = hospital2.doctors.create(name: "Tim", speciality: "Attending Surgeon", school: "University of Pennsylvania")
+
+# Patients:
+
+austin = Patient.create(name: "Austin", age: 26)
+jared = Patient.create(name: "Jared", age: 29)
+greg = Patient.create(name: "Greg", age: 28)
+mike = Patient.create(name: "Mike", age: 27)
---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Doctor do
+  describe 'Relationships' do
+    it {should belong_to :hospital}
+    it {should have_many(:patients).through(:doctor_patients)}
+  end
+end
